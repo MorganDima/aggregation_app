7f53dd71db5c30e47f02b37dbb46b1e32d172d02
MorganDima <atmdm@yandex.ru>
2023-12-22 13:01:40 +0200

    my first commit

---
+++ src/main/java/com/example/aggregationapp/AggregationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.aggregationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aggregationapp/AggregationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aggregationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
