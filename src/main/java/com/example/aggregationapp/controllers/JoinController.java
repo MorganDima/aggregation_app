129fb8b1bd1e9a5a7123dd5ecd8c2cdc9e37ca57
dmitr <atmdm@yandex.ru>
2023-12-25 07:33:02 +0200

    second

--- src/main/java/com/example/aggregationapp/controllers/JoinController.java
+++ src/main/java/com/example/aggregationapp/controllers/JoinController.java
@@ -1,16 +1,20 @@
 package com.example.aggregationapp.controllers;
 
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class JoinController {
 
     @PostMapping("/aggregation_app")
     private static String getData() {
 
-        return "Hello";
+        log.info("Hello from first controller");
+
+        return "Hello from aggregation API";
     }
 
 }
