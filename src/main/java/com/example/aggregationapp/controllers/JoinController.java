67c6ec6eddd5be9a7163f4a3c4e96213a48f9ca8
MorganDima <atmdm@yandex.ru>
2023-12-26 19:04:54 +0200

    my commit

--- src/main/java/com/example/aggregationapp/controllers/JoinController.java
+++ src/main/java/com/example/aggregationapp/controllers/JoinController.java
@@ -1,16 +1,39 @@
 package com.example.aggregationapp.controllers;
 
 
+import com.example.aggregationapp.dto.UserDto;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class JoinController {
 
     @PostMapping("/aggregation_app")
-    private static String getData() {
+    public ResponseEntity<String> getData() {
 
-        return "Hello";
+        log.info("Hello from Aggregation APP, get data");
+
+        String jsonResponse = "{\"message\": \"Hello from Aggregation APP\"}";
+
+        return ResponseEntity.status(HttpStatus.OK).body(jsonResponse);
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<String> postData(@RequestBody UserDto userDto) {
+
+
+        log.info("User dto " + userDto.toString());
+
+        String jsonResponse = "{\"message\": \"User " + userDto.getUserName() + "had been created\"}";
+
+        return ResponseEntity.status(HttpStatus.OK).body(jsonResponse);
+
+    }
 }
---
+++ src/main/java/com/example/aggregationapp/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.aggregationapp.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String email;
+    private String password;
+    private String userName;
+    private String fullName;
+
+}
